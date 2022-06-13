0fe563c31b631eaa6eb7d6f7ec918fb614d04ca0
y3ll0w42 <y3ll0w42.dev@gmail.com>
2022-06-13 14:39:54 +0200

    boot like Philipp Oppermann

--- srcs/main.rs
+++ srcs/main.rs
@@ -1,5 +1,6 @@
 #![feature(lang_items)]
 #![no_std]
+extern crate rlibc;
 
 use core::panic::PanicInfo;
 
@@ -8,18 +9,21 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+	loop {}
 }
 
-extern "C" {
-    fn _print();
-}
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
-pub extern fn rust_main() {
-    let x = ["hello", "World", "!"];
-    let y =x;
-    unsafe {_print();}
+pub extern fn rust_main() -> ! {
+	let vga_buffer = 0xb8000 as *mut u8;
+
+	for (i, &byte) in HELLO.iter().enumerate() {
+		unsafe {
+			*vga_buffer.offset(i as isize * 2) = byte;
+			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+		}
+	}
+
+	loop {}
 }
-
-
