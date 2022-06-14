03003c71cb8a348d7a748da2d5f2c5b864469159
y3ll0w42 <y3ll0w42.dev@gmail.com>
2022-06-14 13:20:43 +0200

    typo

--- srcs/boot/header.s
+++ srcs/boot/header.s
@@ -5,7 +5,7 @@ header_start:
 	dd MULTIBOOT_MAGIC ; magic
 	dd 0 ; architecture - 0: protected mode
 	dd header_end - header_start ; header length
-	dd 0x100000000 - (MAGIC + 0 + (header_end - header_start)) ; checksum
+	dd 0x100000000 - (MULTIBOOT_MAGIC + 0 + (header_end - header_start)) ; checksum
 	dw 0 ; type
 	dw 0 ; flags
 	dd 8 ; size
