8e8eb1e6e7b67538b1c445b0ed4e1e91042ddb8d
Harthann <nca.eyraud@gmail.com>
2022-07-11 18:41:33 +0200

    adding new_entry who map phys addr to virt addr

--- srcs/main.rs
+++ srcs/main.rs
@@ -34,15 +34,17 @@ pub extern "C" fn eh_personality() {}
 
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
-	unsafe{
-		let page_dir: *mut PageDirectory = page_directory as *mut _;
-		kprintln!("PageDir entry[0]: {}", (*page_dir).entries[0]);
-		kprintln!("PageDir entry[768]: {}", (*page_dir).entries[768]);
-		kprintln!("PageDir entry[1]: {}", (*page_dir).entries[1]);
-		let page_tab: *mut PageTable = (*page_dir).entries[0].get_address() as *mut _;
+	unsafe {
+		let mut page_dir: &mut PageDirectory = &mut *(page_directory as *mut _);
+		kprintln!("PageDir entry[0]: {}", page_dir.entries[0]);
+		kprintln!("PageDir entry[768]: {}", page_dir.entries[768]);
+		kprintln!("PageDir entry[1]: {}", page_dir.entries[1]);
+		let page_tab: *mut PageTable = page_dir.entries[0].get_address() as *mut _;
 		kprintln!("PageTable entry[0]: {}", (*page_tab).entries[0]);
 		kprintln!("PageTable entry[1]: {}", (*page_tab).entries[1]);
 		kprintln!("PageTable entry[2]: {}", (*page_tab).entries[2]);
+		(*page_tab).entries[769].free_entry();
+		kprintln!("New page frame: {:#x}", page_dir.new_entry(0x5000000).unwrap());
 	}
 /*
 	let ptr = 0xdeadbeaf as *mut u32;
@@ -55,7 +57,7 @@ pub extern "C" fn kmain() -> ! {
 	change_color!(Color::White, Color::Black);
 
 	//kprintln!("Stack bottom: {:x}\nStack top:{:x}\nStart: {:x}\nRust main {:x}", stack_bottom as u32, stack_top as u32, _start as u32, rust_start as u32);
-	hexdump!(0x800 as *mut _, unsafe{gdt_desc as usize});
+//	hexdump!(0x800 as *mut _, unsafe{gdt_desc as usize});
 	kprint!("$> ");
 	loop {
 		if keyboard::keyboard_event() {
--- srcs/paging/mod.rs
+++ srcs/paging/mod.rs
@@ -1,5 +1,8 @@
 use core::fmt;
 
+type phys_addr = u32;
+type virt_addr = u32;
+
 #[repr(align(4096))]
 pub struct PageDirectory {
 	pub entries: [PageDirectoryEntry; 1024]
@@ -8,6 +11,21 @@ pub struct PageDirectory {
 impl PageDirectory {
 	pub const fn new() -> Self {
 		Self {entries: [PageDirectoryEntry::new(0x00000002); 1024]}
+	}
+
+	pub fn new_entry(&self, page_frame: phys_addr) -> Result<virt_addr, ()> {
+		let mut i:usize = 0;
+
+		while i < 1024 {
+			if self.entries[i].get_present() == 1 {
+				let res = self.entries[i].to_page_table().new_frame(page_frame);
+				if  res.is_ok() {
+					return Ok(((i as virt_addr)<< 22) | ((res.unwrap() as virt_addr) << 12));
+				}
+			}
+			i += 1;
+		}
+		Err(())
 	}
 }
 
@@ -21,80 +39,8 @@ impl PageDirectoryEntry {
 		Self {value: value}
 	}
 
-	pub fn get_present(&self) -> u8 {
-		(self.value & 0b00000001) as u8
-	}
-
-	pub fn get_writable(&self) -> u8 {
-		((self.value & 0b00000010) >> 1) as u8
-	}
-
-	pub fn get_user_supervisor(&self) -> u8 {
-		((self.value & 0b00000100) >> 2) as u8
-	}
-
-	pub fn get_pwt(&self) -> u8 {
-		((self.value & 0b00001000) >> 3) as u8
-	}
-
-	pub fn get_pcd(&self) -> u8 {
-		((self.value & 0b00010000) >> 4) as u8
-	}
-
-	pub fn get_accessed(&self) -> u8 {
-		((self.value & 0b00100000) >> 5) as u8
-	}
-
-	pub fn get_ps(&self) -> u8 {
-		((self.value & 0b10000000) >> 7) as u8
-	}
-
-	pub fn get_dirty(&self) -> u8 {
-		if self.get_ps() == 0 {
-			panic!();
-		} else {
-			((self.value & 0b01000000) >> 6) as u8
-		}
-	}
-
-	pub fn get_global(&self) -> u8 {
-		if self.get_ps() == 0 {
-			panic!();
-		} else {
-			((self.value & 0b100000000) >> 8) as u8
-		}
-	}
-
-	pub fn get_avl(&self) -> u8 {
-		if self.get_ps() == 0 {
-			((self.value & 0b111000000000) >> 9) as u8
-		} else {
-			(((self.value & 0b111100000000) >> 8) | ((self.value & 0b01000000) >> 2)) as u8
-		}
-	}
-
-	pub fn get_address(&self) -> u32 {
-		if self.get_ps() == 0 {
-			self.value & 0xfffff000
-		} else {
-			((self.value & 0xfff00000) >> 10) | ((self.value & 0b111111110000000000000) << 19)
-		}
-	}
-
-	pub fn get_pat(&self) -> u8 {
-		if self.get_ps() == 0 {
-			panic!();
-		} else {
-			((self.value & 0b1000000000000) >> 12) as u8
-		}
-	}
-
-	pub fn get_rsvd(&self) -> u8 {
-		if self.get_ps() == 0 {
-			panic!();
-		} else {
-			((self.value & 0b100000000000000000000) >> 20) as u8
-		}
+	pub fn to_page_table(&self) -> &mut PageTable {
+		unsafe{ &mut *(self.get_address() as *mut _)}
 	}
 }
 
@@ -141,18 +87,59 @@ impl PageTable {
 	pub const fn new() -> Self {
 		Self {entries: [PageTableEntry::new(0x0); 1024]}
 	}
+
+	pub fn new_frame(&mut self, page_frame: u32) -> Result<u16, ()> {
+		let mut i: usize = 0;
+
+		while i < 1024 {
+			if self.entries[i].get_present() != 1 {
+				self.entries[i] = (page_frame | 3).into();
+				return Ok(i as u16);
+			}
+			i += 1;
+		}
+		Err(())
+	}
+
 }
 
 #[derive(Copy, Clone)]
+#[repr(transparent)]
 pub struct PageTableEntry {
 	value: u32
+}
+
+impl From<u32> for PageTableEntry {
+	fn from(item: u32) -> Self {
+		PageTableEntry { value: item }
+	}
 }
 
 impl PageTableEntry {
 	pub const fn new(value: u32) -> Self {
 		Self {value: value}
 	}
+}
 
+impl fmt::Display for PageTableEntry {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "Present: {}
+Writable: {}
+User/Supervisor: {}
+PWT: {}
+PCD: {}
+Accessed: {}
+Dirty: {}
+PAT: {}
+Global: {}
+AVL: 0x{:x}
+Address: {:#010x}", self.get_present(), self.get_writable(), self.get_user_supervisor(),
+self.get_pwt(), self.get_pcd(), self.get_accessed(), self.get_dirty(), self.get_pat(),
+self.get_global(), self.get_avl(), self.get_address())
+	}
+}
+
+impl PageTableEntry {
 	pub fn get_present(&self) -> u8 {
 		(self.value & 0b00000001) as u8
 	}
@@ -196,22 +183,87 @@ impl PageTableEntry {
 	pub fn get_address (&self) -> u32 {
 		self.value & 0xfffff000
 	}
-}
 
-impl fmt::Display for PageTableEntry {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "Present: {}
-Writable: {}
-User/Supervisor: {}
-PWT: {}
-PCD: {}
-Accessed: {}
-Dirty: {}
-PAT: {}
-Global: {}
-AVL: 0x{:x}
-Address: {:#010x}", self.get_present(), self.get_writable(), self.get_user_supervisor(),
-self.get_pwt(), self.get_pcd(), self.get_accessed(), self.get_dirty(), self.get_pat(),
-self.get_global(), self.get_avl(), self.get_address())
+	pub fn free_entry(&mut self) {
+		self.value =0 ;
 	}
 }
+
+impl PageDirectoryEntry {
+	pub fn get_present(&self) -> u8 {
+		(self.value & 0b00000001) as u8
+	}
+
+	pub fn get_writable(&self) -> u8 {
+		((self.value & 0b00000010) >> 1) as u8
+	}
+
+	pub fn get_user_supervisor(&self) -> u8 {
+		((self.value & 0b00000100) >> 2) as u8
+	}
+
+	pub fn get_pwt(&self) -> u8 {
+		((self.value & 0b00001000) >> 3) as u8
+	}
+
+	pub fn get_pcd(&self) -> u8 {
+		((self.value & 0b00010000) >> 4) as u8
+	}
+
+	pub fn get_accessed(&self) -> u8 {
+		((self.value & 0b00100000) >> 5) as u8
+	}
+
+	pub fn get_ps(&self) -> u8 {
+		((self.value & 0b10000000) >> 7) as u8
+	}
+
+	pub fn get_dirty(&self) -> u8 {
+		if self.get_ps() == 0 {
+			todo!();
+		} else {
+			((self.value & 0b01000000) >> 6) as u8
+		}
+	}
+
+	pub fn get_global(&self) -> u8 {
+		if self.get_ps() == 0 {
+			todo!();
+		} else {
+			((self.value & 0b100000000) >> 8) as u8
+		}
+	}
+
+	pub fn get_avl(&self) -> u8 {
+		if self.get_ps() == 0 {
+			((self.value & 0b111000000000) >> 9) as u8
+		} else {
+			(((self.value & 0b111100000000) >> 8) | ((self.value & 0b01000000) >> 2)) as u8
+		}
+	}
+
+	pub fn get_address(&self) -> u32 {
+		if self.get_ps() == 0 {
+			self.value & 0xfffff000
+		} else {
+			((self.value & 0xfff00000) >> 10) | ((self.value & 0b111111110000000000000) << 19)
+		}
+	}
+
+	pub fn get_pat(&self) -> u8 {
+		if self.get_ps() == 0 {
+			todo!();
+		} else {
+			((self.value & 0b1000000000000) >> 12) as u8
+		}
+	}
+
+	pub fn get_rsvd(&self) -> u8 {
+		if self.get_ps() == 0 {
+			todo!();
+		} else {
+			((self.value & 0b100000000000000000000) >> 20) as u8
+		}
+	}
+}
+
