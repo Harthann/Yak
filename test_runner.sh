63b84a819c204324c14e4b9832e845b90e63c5c9
root <root@vps-93d2a5ac.vps.ovh.net>
2022-07-20 16:24:29 +0000

    fix test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -6,7 +6,7 @@ echo "Running kfs unit tests"
 cp $1 iso/boot/$2
 
 # Changing grub timeout
-if [[ $(uname) -eq "Darwin" ]]; then
+if [ "$(uname)" == "Darwin" ]; then
 	sed -i '' "s/timeout=./timeout=0/" iso/boot/grub/grub.cfg
 else
 	sed -i "s/timeout=./timeout=0/" iso/boot/grub/grub.cfg
@@ -22,7 +22,7 @@ qemu-system-i386 -d int \
 			-device isa-debug-exit,iobase=0xf4,iosize=0x04 2> qemu.log
 
 ret=$(echo $?)
-if [[ $ret -eq 33 ]]; then
+if [ $ret -eq 33 ]; then
 	exit 0;
 fi
 exit 1;
