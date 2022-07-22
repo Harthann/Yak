2f7495d3d66526218d07227af65142e82de70699
ska42 <skalyxe00@gmail.com>
2022-07-22 10:11:07 +0200

    remove comments/test on allocator

--- srcs/allocator/mod.rs
+++ srcs/allocator/mod.rs
@@ -36,33 +36,14 @@ fn align_up(addr: VirtAddr, align: usize) -> VirtAddr {
 	(addr + align as u32 - 1) & !(align as u32 - 1)
 }
 
-/*
 pub fn init_heap(heap: u32, allocator) {
->>>>>>> allocator
 	let nb_page: usize = if HEAP_SIZE % 4096 == 0 {HEAP_SIZE / 4096} else {HEAP_SIZE / 4096 + 1};
 	alloc_pages_at_addr(heap, nb_page);
 	unsafe{allocator.init(heap as usize, HEAP_SIZE)};
-	/* TESTS */
-	unsafe {
-		use core::alloc::GlobalAlloc;
-
-		let res = Layout::from_size_align(8, 8);
-		if res.is_ok() {
-			allocator.alloc(res.unwrap());
-		}
-	}
 }
-*/
 
 pub fn init_kheap(heap: VirtAddr, allocator: &mut dyn Allocator) {
 	let nb_page: usize = if KHEAP_SIZE % 4096 == 0 {KHEAP_SIZE / 4096} else {KHEAP_SIZE / 4096 + 1};
 	kalloc_pages_at_addr(heap, nb_page);
 	unsafe{allocator.init(heap, KHEAP_SIZE)};
-	/* TESTS */
-	unsafe {
-		let res = Layout::from_size_align(8, 8);
-		if res.is_ok() {
-			allocator.alloc(res.unwrap());
-		}
-	}
 }
