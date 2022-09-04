a2cb19e9d98edb749b5e6ccf2f9ec3b06fb78467
y3ll0w42 <y3ll0w42.dev@gmail.com>
2022-09-04 12:17:50 +0200

    remove header asm

--- files.mk
+++ files.mk
@@ -15,8 +15,7 @@ RUST_SRCS		=	main.rs \
 
 KERNELSRCS		=	$(foreach file, $(RUST_SRCS), $(shell find $(DIR_SRCS) -name $(file) -type f))
 
-BOOTSRCS		=	header.s \
-					boot.s \
+BOOTSRCS		=	boot.s \
 					gdt.s
 
 BOOTOBJS		=	$(BOOTSRCS:%.s=$(DIR_OBJS)/%.o)
