581d897f4533366e6cd98e9526d7416e6e572271
--replace-all <y3ll0w42.dev@gmail.com>
2022-12-08 17:33:05 +0100

    Add draft process usermode

--- srcs/user/mod.rs
+++ srcs/user/mod.rs
@@ -1,26 +1,81 @@
 //! Testing user space code
 
+use core::ptr::copy_nonoverlapping;
+
+use crate::wrappers::{_cli, _sti};
+
+use crate::memory::{MemoryZone, Heap};
+
 use crate::memory::paging as mem;
 use crate::memory::paging::{
 PAGE_WRITABLE,
 PAGE_USER
 };
+use crate::VirtAddr;
+
+use crate::proc::process::{Process, Pid};
+use crate::proc::task::{Task, TASKLIST};
+
+use crate::memory::paging::page_directory;
+use crate::memory::paging::page_directory::PageDirectory;
+
+use crate::memory::allocator::Box;
 
 extern "C" {
-	fn jump_usermode();
+	fn jump_usermode(func: VirtAddr);
 	fn userfunc();
 	fn userfunc_end();
+}
+
+pub unsafe fn exec_fn_userspace(func: VirtAddr, size: usize) -> Pid {
+	_cli();
+	let running_task: &mut Task = Task::get_running_task();
+	let parent: &mut Process = Process::get_running_process();
+
+	let mut process: Process = Process::new();
+	process.init(parent);
+	process.heap = <MemoryZone as Heap>::init_no_allocator(
+		size,
+		PAGE_WRITABLE | PAGE_USER,
+		false
+	);
+	copy_nonoverlapping(
+		func as *mut u8,
+		process.heap.offset as *mut u8,
+		size
+	);
+	parent.childs.push(Box::new(process));
+
+	let process: &mut Process = parent.childs.last_mut().unwrap();
+	let mut new_task: Task = Task::new();
+	new_task.regs = running_task.regs;
+	new_task.process = process;
+
+	let page_dir: &mut PageDirectory = PageDirectory::new();
+	page_dir.set_entry(0xb0000000 >> 22, get_paddr!(process.stack.offset));
+	page_dir.set_entry(0x08000000 >> 22, get_paddr!(process.heap.offset));
+	new_task.regs.esp -= 4;
+	core::arch::asm!("mov [{esp}], {func}",
+		esp = in(reg) new_task.regs.esp,
+		func = in(reg) func);
+	new_task.regs.esp -= 4;
+	core::arch::asm!("mov [{esp}], {func}",
+		esp = in(reg) new_task.regs.esp,
+		func = in(reg) jump_usermode);
+	TASKLIST.push(new_task);
+	_sti();
+	process.pid
 }
 
 pub fn test_user_page() {
 	let userpage = mem::alloc_pages_at_addr(0x400000, 1, PAGE_WRITABLE | PAGE_USER).expect("");
 	let funclen = userfunc_end as usize - userfunc as usize;
-	
+
 	unsafe {
 		core::ptr::copy_nonoverlapping(userfunc as *const u8, userpage as *mut u8, funclen);
 	}
 	mem::print_pdentry(1);
 	unsafe {
-		jump_usermode();
+		jump_usermode(0x400000);
 	}
 }
--- srcs/user/userjump.s
+++ srcs/user/userjump.s
@@ -2,20 +2,21 @@ global jump_usermode
 global userfunc
 global userfunc_end
 
-jump_usermode:
+jump_usermode:; jump_usermode(func: VirtAddr)
+	mov ebx, dword[esp + 4]
 	mov ax, (5 * 8) | 3 ; ring 3 data with bottom 2 bits set for ring 3
 	mov ds, ax
 	mov es, ax 
 	mov fs, ax 
 	mov gs, ax ; SS is handled by iret
- 
+
 	; set up the stack frame iret expects
 	mov eax, esp
 	push (5 * 8) | 3 ; data selector
 	push eax ; current esp
 	pushf ; eflags
 	push (4 * 8) | 3 ; code selector (ring 3 code with bottom 2 bits set for ring 3)
-	push 0x400000 ; instruction address to return to
+	push ebx ; func
 	iret
 
 userfunc:
