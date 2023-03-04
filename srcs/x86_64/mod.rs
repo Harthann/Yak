c5f56689ea9bd773ae58c466448b1a9ed51e73f2
Harthann <nca.eyraud@gmail.com>
2023-03-04 15:12:16 +0100

    Creation of an interface for Ports in x86_64 architecture. This is based on the work from the x86_64 crate.

---
+++ srcs/x86_64/mod.rs
@@ -0,0 +1,11 @@
+pub mod port;
+
+#[allow(dead_code)]
+pub fn io_wait() {
+    unsafe {
+	    	core::arch::asm!("out dx, eax",
+		        in("dx") 0x80,
+		        in("eax") 0,
+                options(nomem, nostack, preserves_flags));
+	    }
+}
