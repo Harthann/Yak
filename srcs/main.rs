9ddb747df2914219935bd67338ad2793da2ca7df
Harthann <nca.eyraud@gmail.com>
2023-03-25 13:37:20 +0100

    Adding comment on sleep function, fixing sleep issue, was sleep until values where equal but it doesn't work when the timing become imprecise and the value can be overshoot before we check the equality

--- srcs/main.rs
+++ srcs/main.rs
@@ -1,18 +1,20 @@
 use crate::vga_buffer::change_color;
 use crate::vga_buffer::color::Color;
 use crate::{kprint, kprintln, string};
-
 use crate::wrappers::{hlt, cli, sti};
+
+// Temporary sleep function until a proper sleep is implemented and teste
 pub fn sleep(microseconds: usize) {
     unsafe {
     let tmp = crate::pic::JIFFIES;
-    while crate::pic::JIFFIES != tmp + microseconds {
+    while crate::pic::JIFFIES < tmp + microseconds {
         sti!();
         hlt!();
         cli!();
     }
     }
 }
+
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
 	crate::user::test_user_page();
