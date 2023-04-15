78f46d2c74dd3a4b7b79011d1c1efbb493d5437a
0x050f <0x050f@users.noreply.github.com>
2023-04-15 12:42:14 +0000

    deploy: a3d62f46935e491805390f3d985ef045c6bbf64d

--- kernel/proc/task/sidebar-items.js
+++ kernel/proc/task/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["TaskStatus",""]],"fn":[["_end_handler",""],["do_signal",""],["handle_signal",""],["save_task",""],["schedule_task",""],["switch_task",""],["wrapper_handler",""]],"static":[["TASKLIST",""]],"struct":[["Task",""]]};
+window.SIDEBAR_ITEMS = {"enum":[["TaskStatus",""]],"fn":[["_end_handler",""],["do_signal",""],["handle_signal",""],["save_task",""],["schedule_task",""],["switch_task",""],["wrapper_handler",""]],"static":[["TASKLIST",""],["tmp_registers",""]],"struct":[["Task",""]]};
