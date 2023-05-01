4d375669c10a1b6bc415b254c27e83f86d99276a
Harthann <nca.eyraud@gmail.com>
2023-05-01 15:21:02 +0200

    Better doc for Flags

--- srcs/utils/flags.rs
+++ srcs/utils/flags.rs
@@ -7,84 +7,84 @@ pub struct Flags<T>(pub T);
 /// To use this trait the type T has to allow interior mutability
 /// This is ok in the example since AtomicU8 is thread safe and allow interior mutability
 pub trait FlagOp<T> {
+	fn toggle(&self, bit: u8);
+	fn enable(&self, bit: u8);
+	fn disable(&self, bit: u8);
+	fn is_enable(&self, bit: u8) -> bool;
+	fn is_disable(&self, bit: u8) -> bool;
+}
+
+impl FlagOp<AtomicU8> for Flags<AtomicU8> {
 	/// Toggle bit.
-	/// Safety: No check performed, will produce panic if trying to toggle bit outside of range
 	///
-	/// e.g:
+	/// # Safety
+	/// No check performed, will produce panic if trying to toggle bit outside of range
+	///
+	/// # Examples
 	///
 	/// ```
 	/// let flag = Flags::<AtomicU8>::default();
 	/// flag.toggle(1); // Toggle the first bit of the inner AtomicU8
 	/// flag.toggle(9); // Panic there is no 9th bit in an u8
 	/// ```
-	fn toggle(&self, bit: u8);
+	fn toggle(&self, bit: u8) {
+		self.0.fetch_xor(1 << bit, Ordering::Relaxed);
+	}
 
 	/// enable x bit.
-	/// Safety: No check performed, will produce panic if trying to enable bit outside of range
+	/// # Safety
+	/// No check performed, will produce panic if trying to enable bit outside of range
 	///
-	/// e.g:
+	/// # Examples
 	///
 	/// ```
 	/// let flag = Flags::<AtomicU8>::default();
 	/// flag.disable(1); // Enable the first bit of the inner AtomicU8
 	/// flag.disable(9); // Panic there is no 9th bit in an u8
 	/// ```
-	fn enable(&self, bit: u8);
+	fn enable(&self, bit: u8) {
+		self.0.fetch_or(1 << bit, Ordering::Relaxed);
+	}
 
 	/// disable x bit.
-	/// Safety: No check performed, will produce panic if trying to disable bit outside of range
+	/// # Safety
+	/// No check performed, will produce panic if trying to disable bit outside of range
 	///
-	/// e.g:
+	/// # Examples
 	///
 	/// ```
 	/// let flag = Flags::<AtomicU8>::default();
 	/// flag.disable(1); // Disable the first bit of the inner AtomicU8
 	/// flag.disable(9); // Panic there is no 9th bit in an u8
 	/// ```
-	fn disable(&self, bit: u8);
+	fn disable(&self, bit: u8) {
+		self.0.fetch_and(!(1 << bit), Ordering::Relaxed);
+	}
 
 	/// check if the x bit is set.
 	/// safety: no check performed, will produce panic if trying to check bit outside of range
 	///
-	/// e.g:
+	/// # Examples
 	///
 	/// ```
 	/// let flag = flags::<atomicu8>::default();
 	/// flag.is_enable(1); // return true/false depending of the firt bit state
 	/// flag.is_enable(9); // panic there is no 9th bit in an u8
 	/// ```
-	fn is_enable(&self, bit: u8) -> bool;
+	fn is_enable(&self, bit: u8) -> bool {
+		(self.0.load(Ordering::Relaxed) & (1 << bit)) == (1 << bit)
+	}
 
 	/// check if the x bit is unset.
 	/// safety: no check performed, will produce panic if trying to check bit outside of range
 	///
-	/// e.g:
+	/// # Examples
 	///
 	/// ```
 	/// let flag = flags::<atomicu8>::default();
 	/// flag.is_disable(1); // return true/false depending of the firt bit state
 	/// flag.is_disable(9); // panic there is no 9th bit in an u8
 	/// ```
-	fn is_disable(&self, bit: u8) -> bool;
-}
-
-impl FlagOp<AtomicU8> for Flags<AtomicU8> {
-	fn toggle(&self, bit: u8) {
-		self.0.fetch_xor(1 << bit, Ordering::Relaxed);
-	}
-
-	fn enable(&self, bit: u8) {
-		self.0.fetch_or(1 << bit, Ordering::Relaxed);
-	}
-
-	fn disable(&self, bit: u8) {
-		self.0.fetch_and(!(1 << bit), Ordering::Relaxed);
-	}
-
-	fn is_enable(&self, bit: u8) -> bool {
-		(self.0.load(Ordering::Relaxed) & (1 << bit)) == (1 << bit)
-	}
-
 	fn is_disable(&self, bit: u8) -> bool {
 		!self.is_enable(bit)
 	}
