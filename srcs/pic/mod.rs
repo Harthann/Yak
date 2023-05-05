93064eb491a6130f973be182a0e07198202a68a3
Harthann <nca.eyraud@gmail.com>
2023-05-05 10:16:31 +0200

    Adding no_mangle to end_of_interrupt for release version

--- srcs/pic/mod.rs
+++ srcs/pic/mod.rs
@@ -73,6 +73,7 @@ pub const PIC2_IRQ_SECATA: u8 = PIC2_IRQ_OFFSET + 7;
 const PIC_EOI: u8 = 0x20; // End of Interrupts command code
 
 #[inline]
+#[no_mangle]
 pub fn end_of_interrupts(irq: usize) {
 	if irq >= 8 {
 		// Slave interrupt request
