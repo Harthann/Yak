b8c6015e46a812dbd6be8933abeeb4b06dbd7167
0x050f <47715948+0x050f@users.noreply.github.com>
2023-05-18 09:49:01 +0200

    Fix stack on pic interrupts before kmain

--- srcs/kinit.rs
+++ srcs/kinit.rs
@@ -158,10 +158,10 @@ pub extern "C" fn kinit() {
 	// Setting up frequency divider to modulate IRQ0 rate, low value tends to get really slow (too much task switching
 	// This setup should be done using frequency, but for readability and ease of use, this is done
 	// with time between each interrupt in ms.
-	pic::set_irq0_in_ms(10.0);
+	pic::set_irq0_in_ms(1.0);
 
 	// Reserve some spaces to push things before main
-	unsafe { core::arch::asm!("mov esp, {}", in(reg) STACK_ADDR + 1) };
+	unsafe { core::arch::asm!("mov esp, {}", in(reg) STACK_ADDR + 1 - 32) };
 	crate::wrappers::_sti();
 
 	#[cfg(test)]
