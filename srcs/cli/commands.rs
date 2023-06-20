6cb38923e39bf606c51bc9c151e29e99ca8004c8
Harthann <nca.eyraud@gmail.com>
2023-06-20 13:14:30 +0200

    Formatting

--- srcs/cli/commands.rs
+++ srcs/cli/commands.rs
@@ -45,45 +45,46 @@ pub fn command_entry(cmd_id: usize, ptr: *mut String, len: usize, cap: usize) {
 }
 
 fn valgrind(command: Vec<String>) {
-    if command.len() < 2 {
+	if command.len() < 2 {
 		kprintln!("Invalid argument.");
 		kprintln!("Usage: valgrind [command] [command args]");
 		return;
 	}
 
-    // Save current heap state
-    let heap_state = unsafe {crate::KTRACKER};
+	// Save current heap state
+	let heap_state = unsafe { crate::KTRACKER };
 
-    let sub_command = &command[1..command.len()];
-    let cmd_id = KNOWN_CMD.iter().position(|&x| x == sub_command[0]);
-    match cmd_id {
-        Some(index) => COMMANDS[index](sub_command.to_vec()),
-        None => kprintln!("Command: [{}] not found", sub_command[0])
-    }
+	let sub_command = &command[1..command.len()];
+	let cmd_id = KNOWN_CMD.iter().position(|&x| x == sub_command[0]);
+	match cmd_id {
+		Some(index) => COMMANDS[index](sub_command.to_vec()),
+		None => kprintln!("Command: [{}] not found", sub_command[0])
+	}
 
-    let mut current_state = unsafe {crate::KTRACKER};
+	let mut current_state = unsafe { crate::KTRACKER };
 	current_state.allocation -= heap_state.allocation;
-    current_state.allocated_bytes -= heap_state.allocated_bytes;
+	current_state.allocated_bytes -= heap_state.allocated_bytes;
 	current_state.freed -= heap_state.freed;
 	current_state.freed_bytes -= heap_state.freed_bytes;
 	crate::kprintln!("{}", current_state);
-    if current_state.allocated_bytes < current_state.freed_bytes {
-        crate::kprintln!(
-            "Too much bytes freed: {}",
-            current_state.freed_bytes - current_state.allocated_bytes
-        );
-    } else {
-	    crate::kprintln!(
-	    	"Leaks: {} bytes",
-	    	current_state.allocated_bytes - current_state.freed_bytes
-	    );
-    }
+	if current_state.allocated_bytes < current_state.freed_bytes {
+		crate::kprintln!(
+			"Too much bytes freed: {}",
+			current_state.freed_bytes - current_state.allocated_bytes
+		);
+	} else {
+		crate::kprintln!(
+			"Leaks: {} bytes",
+			current_state.allocated_bytes - current_state.freed_bytes
+		);
+	}
 }
 
 fn pmap(command: Vec<String>) {
 	let pid: Pid;
 
-	if command.len() != 2 { kprintln!("Invalid argument.");
+	if command.len() != 2 {
+		kprintln!("Invalid argument.");
 		kprintln!("Usage: pmap [pid]");
 		return;
 	}
