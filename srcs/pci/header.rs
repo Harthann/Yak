7d4eeadce2b3c013adc766b84fc6b5888ae04cd8
Harthann <nca.eyraud@gmail.com>
2023-06-27 15:12:45 +0200

    Removing command and status registers structure

--- srcs/pci/header.rs
+++ srcs/pci/header.rs
@@ -52,22 +52,6 @@ pub struct PciCommonHdr {
 	pub bist:            u8
 }
 
-/// Represent the status register of a pci device, allowing friendly interface to read/write bit on
-/// this register
-pub struct StatusRegister(u16);
-
-/// Represent the command register of a pci device, allowing friendly interface to read/write bit on
-/// this register
-pub struct CommandRegister(u16);
-
-impl StatusRegister {
-	pub const PIN0: u16 = 1 << 0;
-
-	pub fn set(&mut self, pin: u16) {
-		self.0 |= pin;
-	}
-}
-
 /// Header layout for standard Pci
 #[derive(Debug, Default)]
 pub struct StandardHdr {
