61a055f03bd15e34214a162894a794ca40c66255
Harthann <nca.eyraud@gmail.com>
2023-06-30 15:41:32 +0200

    Fomratting

--- srcs/syscalls/mmap.rs
+++ srcs/syscalls/mmap.rs
@@ -1,58 +1,63 @@
 use crate::memory::paging;
-use crate::memory::paging::{page_directory, PAGE_PRESENT, PAGE_WRITABLE, page_table::PageTable};
-
+use crate::memory::paging::page_table::PageTable;
+use crate::memory::paging::{page_directory, PAGE_PRESENT, PAGE_WRITABLE};
 
 #[repr(C)]
 #[derive(Debug)]
 pub struct mmap_arg {
-    addr:   usize,
-    length: usize,
-    prot:   u32,
-    flags:  u32,
-    fd:     i32,
-    offset: usize
+	addr:   usize,
+	length: usize,
+	prot:   u32,
+	flags:  u32,
+	fd:     i32,
+	offset: usize
 }
 
 /// Translate VirtAddr from userspace to kernelspace
 /// This can be optimize a lot.
 pub fn translate_vaddr(addr: VirtAddr) -> VirtAddr {
-    unsafe {
-	let binding = Process::get_running_process();
-	let curr_process = binding.lock();
+	unsafe {
+		let binding = Process::get_running_process();
+		let curr_process = binding.lock();
 
-    let pd_index = (addr as usize >> 22) as usize;
-    let pt_index = ((addr as usize & 0x3ff000) >> 12) as usize;
-    let pt_paddr = (*curr_process.pd).get_entry(pd_index).get_paddr();
-    for i in &curr_process.page_tables {
-        if get_paddr!(i.get_vaddr()) == pt_paddr {
-            let pdik = i.get_vaddr() as usize >> 22;
-            let page_paddr = i.entries[pt_index].get_paddr();
-            for j in 0..1024 {
-                    if page_directory.get_entry(j).get_present() == 1 {
-                        for k in 0..1024 {
-                            let pt = page_directory.get_page_table(j);
-                            if pt.entries[k].get_paddr() == page_paddr {
-                                crate::dprintln!("Found: {:#x} {} {}", get_vaddr!(j, k), j, k);
-                                return get_vaddr!(j, k);
-                            }
-                        }
-                    }
-            }
-            return 0x0;
-        }
-    }
-    return 0x0;
-    }
+		let pd_index = (addr as usize >> 22) as usize;
+		let pt_index = ((addr as usize & 0x3ff000) >> 12) as usize;
+		let pt_paddr = (*curr_process.pd).get_entry(pd_index).get_paddr();
+		for i in &curr_process.page_tables {
+			if get_paddr!(i.get_vaddr()) == pt_paddr {
+				let pdik = i.get_vaddr() as usize >> 22;
+				let page_paddr = i.entries[pt_index].get_paddr();
+				for j in 0..1024 {
+					if page_directory.get_entry(j).get_present() == 1 {
+						for k in 0..1024 {
+							let pt = page_directory.get_page_table(j);
+							if pt.entries[k].get_paddr() == page_paddr {
+								crate::dprintln!(
+									"Found: {:#x} {} {}",
+									get_vaddr!(j, k),
+									j,
+									k
+								);
+								return get_vaddr!(j, k);
+							}
+						}
+					}
+				}
+				return 0x0;
+			}
+		}
+		return 0x0;
+	}
 }
 
 pub fn mmap(addr: *const mmap_arg) -> *const u8 {
 	//! WARNING: Currently flags sended to MemoryZone are ignored and used to give PageDirectory
 	//! the page flags. The flag PAGE_USER may overlap with one of mmap flags
 
-    let translated_addr = translate_vaddr(addr as VirtAddr);
-    let translated_addr = (translated_addr as usize) + (addr as usize & 0xfff);
+	let translated_addr = translate_vaddr(addr as VirtAddr);
+	let translated_addr = (translated_addr as usize) + (addr as usize & 0xfff);
 
-    let ptr = unsafe {&*(translated_addr as *const mmap_arg) } ;
+	let ptr = unsafe { &*(translated_addr as *const mmap_arg) };
 	let mz = sys_mmap(
 		ptr.addr as VirtAddr,
 		ptr.length,
@@ -65,36 +70,51 @@ pub fn mmap(addr: *const mmap_arg) -> *const u8 {
 	let binding = Process::get_running_process();
 	let mut curr_process = binding.lock();
 
-    let user_pd = unsafe{ &mut *curr_process.pd};
+	let user_pd = unsafe { &mut *curr_process.pd };
 
-    if mz.is_err() {
-        return 0xff as *const u8;
-    }
-    let mz = mz.unwrap();
-    let offset = mz.lock().offset;
-    if user_pd.get_entry(991).get_present() == 1 {
-        let pt_paddr = user_pd.get_entry(991).get_paddr();
-        for i in &mut curr_process.page_tables {
-            if pt_paddr == unsafe{get_paddr!(i.get_vaddr())} {
-                let pt_index = i.new_frames(unsafe{get_paddr!(offset)}, (size / 4096) as u32, ptr.flags | paging::PAGE_USER);
-                return match pt_index {
-                    Ok(index) => get_vaddr!(991, index as usize) as *const u8,
-                    Err(_) => 0xff as *const u8
-                };
-            }
-        }
-        // try map in pt
-        // if fail try next
-        todo!()
-    } else {
-        let pt: &'static mut PageTable = PageTable::new();
-        unsafe {
-        let pt_index = pt.new_frames(get_paddr!(offset), (size / 4096) as u32, ptr.flags | paging::PAGE_USER).expect("Mmap failed to create first PageTable");
-        user_pd.set_entry(991, get_paddr!(pt as *const _) | PAGE_WRITABLE | PAGE_PRESENT | paging::PAGE_USER);
-        curr_process.page_tables.push(pt);
-        return get_vaddr!(991, pt_index) as *const u8;
-        }
-    }
+	if mz.is_err() {
+		return 0xff as *const u8;
+	}
+	let mz = mz.unwrap();
+	let offset = mz.lock().offset;
+	if user_pd.get_entry(991).get_present() == 1 {
+		let pt_paddr = user_pd.get_entry(991).get_paddr();
+		for i in &mut curr_process.page_tables {
+			if pt_paddr == unsafe { get_paddr!(i.get_vaddr()) } {
+				let pt_index = i.new_frames(
+					unsafe { get_paddr!(offset) },
+					(size / 4096) as u32,
+					ptr.flags | paging::PAGE_USER
+				);
+				return match pt_index {
+					Ok(index) => get_vaddr!(991, index as usize) as *const u8,
+					Err(_) => 0xff as *const u8
+				};
+			}
+		}
+		// try map in pt
+		// if fail try next
+		todo!()
+	} else {
+		let pt: &'static mut PageTable = PageTable::new();
+		unsafe {
+			let pt_index = pt
+				.new_frames(
+					get_paddr!(offset),
+					(size / 4096) as u32,
+					ptr.flags | paging::PAGE_USER
+				)
+				.expect("Mmap failed to create first PageTable");
+			user_pd.set_entry(
+				991,
+				get_paddr!(pt as *const _)
+					| PAGE_WRITABLE | PAGE_PRESENT
+					| paging::PAGE_USER
+			);
+			curr_process.page_tables.push(pt);
+			return get_vaddr!(991, pt_index) as *const u8;
+		}
+	}
 }
 
 use crate::memory::{MemoryZone, TypeZone, VirtAddr};
@@ -129,8 +149,8 @@ pub fn sys_mmap(
 pub fn sys_munmap(addr: *const usize, length: usize) -> i32 {
 	let size = length + (4096 - (length % 4096));
 
-    let translated_addr = translate_vaddr(addr as VirtAddr);
-    let translated_addr = (translated_addr as usize) + (addr as usize & 0xfff);
+	let translated_addr = translate_vaddr(addr as VirtAddr);
+	let translated_addr = (translated_addr as usize) + (addr as usize & 0xfff);
 
 	// Bind and lock the current process
 	let binding = Process::get_running_process();
@@ -153,7 +173,7 @@ pub fn sys_munmap(addr: *const usize, length: usize) -> i32 {
 	let mz = split_list.pop_front();
 
 	// Bind and lock the concerned MemoryZone
-    // This cause panic if we try to unmap non mapped MemoryZone
+	// This cause panic if we try to unmap non mapped MemoryZone
 	let mzbinding = mz.unwrap();
 	let mut guard = mzbinding.lock();
 
